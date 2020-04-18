9f8bfea82a3b218799d921eb092621e9f7b1200f
Edgars Hartmanis <edgarhart@inbox.lv>
2020-04-19 00:28:14 +0300

    exit action performed listner

--- src/pages/MainPage.java
+++ src/pages/MainPage.java
@@ -10,11 +10,11 @@ import java.sql.SQLException;
 public class MainPage {
 	private JFrame frame = new JFrame("Library UI");
 	private JTabbedPane tabbedPane = new JTabbedPane();
-	
-	MainPage(User user) throws SQLException {
+
+	public MainPage(User user) throws SQLException {
+		frame.setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
 		frame.setLayout(new GridLayout(1,1));
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-			
+		
 		JComponent libaryPanel = new LibraryPage().renderLibraryPanel(user);
 		tabbedPane.addTab("Library", null, libaryPanel, "Library");
 		tabbedPane.setMnemonicAt(0, KeyEvent.VK_1);
@@ -26,10 +26,26 @@ public class MainPage {
 		JComponent informationPanel = new InformationPage().renderInformationPage();
 		tabbedPane.addTab("Information", null, informationPanel, "Information");
 		tabbedPane.setMnemonicAt(2, KeyEvent.VK_3);
-		
+
 		frame.add(tabbedPane);
 		frame.setResizable(false);
 		frame.setSize(950, 500);
 		frame.setVisible(true);
+		
+		frame.addWindowListener(new java.awt.event.WindowAdapter() {
+		    @Override
+		    public void windowClosing(java.awt.event.WindowEvent windowEvent) {
+		        if (JOptionPane.showConfirmDialog(frame, 
+		            "Are you sure you want to close this window?", "Close Window?", 
+		            JOptionPane.YES_NO_OPTION,
+		            JOptionPane.QUESTION_MESSAGE) == JOptionPane.YES_OPTION){
+		        	LoginPage login = new LoginPage();
+		        	login.setSize(400,100);
+		        	login.setVisible(true);
+		        	
+		            frame.dispose();
+		        }
+		    }
+		});
 	}
 }
